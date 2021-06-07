28995ff5f3c1b0ff80784b254857a1e776b05332
Enjoys <deadly.info@gmail.com>
2021-06-07 11:30:03 +0300

    pagination for admin products

--- src/Models/Admin/Product/Index.php
+++ src/Models/Admin/Product/Index.php
@@ -6,23 +6,41 @@ namespace EnjoysCMS\Module\Catalog\Models\Admin\Product;
 
 use App\Module\Admin\Core\ModelInterface;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Enjoys\Http\ServerRequestInterface;
+use EnjoysCMS\Core\Components\Pagination\Pagination;
 use EnjoysCMS\Module\Catalog\Entities\Product;
 
 final class Index implements ModelInterface
 {
 
     private EntityManager $entityManager;
+    private ServerRequestInterface $serverRequest;
+    /**
+     * @var \EnjoysCMS\Module\Catalog\Repositories\Product
+     */
+    private $productRepository;
 
-    public function __construct(EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager, ServerRequestInterface $serverRequest)
     {
         $this->entityManager = $entityManager;
+        $this->serverRequest = $serverRequest;
+        $this->productRepository = $entityManager->getRepository(Product::class);
     }
 
     public function getContext(): array
     {
-
+        $pagination = new Pagination($this->serverRequest->get('page', 1), 10);
+        $qb = $this->productRepository->getFindAllQuery();
+        $qb
+            ->setFirstResult($pagination->getOffset())
+            ->setMaxResults($pagination->getLimitItems())
+        ;
+        $result = new Paginator($qb);
+        $pagination->setTotalItems($result->count());
         return [
-            'products' => $this->entityManager->getRepository(Product::class)->findAll()
+            'products' => $result,
+            'pagination' => $pagination,
         ];
     }
 }
