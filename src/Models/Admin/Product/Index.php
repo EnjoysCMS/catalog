8c2a6ce9d28cef60fe1ce3d0e77a6405a199690b
enjoys <deadly.info@gmail.com>
2021-10-14 22:25:48 +0300

    EC-10 order by product id desc in admin list page

--- src/Models/Admin/Product/Index.php
+++ src/Models/Admin/Product/Index.php
@@ -6,36 +6,33 @@ namespace EnjoysCMS\Module\Catalog\Models\Admin\Product;
 
 use App\Module\Admin\Core\ModelInterface;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ObjectRepository;
 use Enjoys\Http\ServerRequestInterface;
 use EnjoysCMS\Core\Components\Pagination\Pagination;
 use EnjoysCMS\Module\Catalog\Entities\Product;
+use EnjoysCMS\Module\Catalog\Repositories;
 
 final class Index implements ModelInterface
 {
 
-    private EntityManager $entityManager;
-    private ServerRequestInterface $serverRequest;
-    /**
-     * @var \EnjoysCMS\Module\Catalog\Repositories\Product
-     */
-    private $productRepository;
+    private Repositories\Product|ObjectRepository|EntityRepository $productRepository;
 
-    public function __construct(EntityManager $entityManager, ServerRequestInterface $serverRequest)
+    public function __construct(private EntityManager $em, private ServerRequestInterface $serverRequest)
     {
-        $this->entityManager = $entityManager;
-        $this->serverRequest = $serverRequest;
-        $this->productRepository = $entityManager->getRepository(Product::class);
+        $this->productRepository = $this->em->getRepository(Product::class);
     }
 
     public function getContext(): array
     {
         $pagination = new Pagination($this->serverRequest->get('page', 1), 10);
-        $qb = $this->productRepository->getFindAllQuery();
-        $qb
+        $qb = $this->productRepository->getFindAllBuilder();
+        $qb->orderBy('p.id', 'desc')
+            ->getQuery()
             ->setFirstResult($pagination->getOffset())
-            ->setMaxResults($pagination->getLimitItems())
-        ;
+            ->setMaxResults($pagination->getLimitItems());
+
         $result = new Paginator($qb);
         $pagination->setTotalItems($result->count());
         return [
