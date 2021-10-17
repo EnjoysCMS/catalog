38bb15705c6f269efa60aade16159b902f57106c
enjoys <deadly.info@gmail.com>
2021-10-17 20:25:22 +0300

    added entity Url

---
+++ src/Entities/Url.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace EnjoysCMS\Module\Catalog\Entities;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="catalog_product_urls")
+ */
+class Url
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private string $path;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $default;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Product")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+
+    public function isDefault(): bool
+    {
+        return $this->default;
+    }
+
+    public function setDefault(bool $default): void
+    {
+        $this->default = $default;
+    }
+
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): void
+    {
+        $this->path = $path;
+    }
+}
