cde179366d5de01e38dc15f3edb56ec7912d80dc
Enjoys <deadly.info@gmail.com>
2021-10-18 15:42:47 +0300

    the delete url

--- src/Controller/Admin/Urls.php
+++ src/Controller/Admin/Urls.php
@@ -15,6 +15,7 @@ use Enjoys\Http\ServerRequestInterface;
 use EnjoysCMS\Core\Components\Helpers\Redirect;
 use EnjoysCMS\Module\Catalog\Entities\Url;
 use EnjoysCMS\Module\Catalog\Helpers\Template;
+use EnjoysCMS\Module\Catalog\Models\Admin\Product\Urls\DeleteUrl;
 use EnjoysCMS\Module\Catalog\Models\Admin\Product\Urls\EditUrl;
 use EnjoysCMS\Module\Catalog\Models\Admin\Product\Urls\MakeDefault;
 use EnjoysCMS\Module\Catalog\Models\Admin\Product\Urls\Manage;
@@ -66,6 +67,23 @@ final class Urls extends BaseController
         return $this->view(
             $this->templatePath . '/product/urls/edit.twig',
             $this->getContext($this->container->get(EditUrl::class))
+        );
+    }
+
+    /**
+     * @Route(
+     *     path="admin/catalog/product/urls/delete",
+     *     name="@a/catalog/product/urls/delete",
+     *     options={
+     *      "aclComment": "[ADMIN] Удаление URL"
+     *     }
+     * )
+     */
+    public function delete()
+    {
+        return $this->view(
+            $this->templatePath . '/product/urls/delete.twig',
+            $this->getContext($this->container->get(DeleteUrl::class))
         );
     }
 
