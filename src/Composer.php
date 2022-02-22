e6c2bbdc68229f46a0ae8b5a371964e9f82d1f7a
Enjoys <deadly.info@gmail.com>
2022-02-22 17:04:11 +0300

    added Composer::assetsInstall

---
+++ src/Composer.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace EnjoysCMS\Module\Catalog;
+
+use Composer\Script\Event;
+
+final class Composer
+{
+    public static function assetsInstall(Event $event)
+    {
+        passthru(sprintf('cd %s && yarn install', realpath(__DIR__.'/..')));
+    }
+
+}
