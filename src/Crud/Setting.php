c1fbc73605683184d491ec3a467fd273fc4a2ed5
enjoys <deadly.info@gmail.com>
2022-04-16 13:32:41 +0300

    adaptive crud/setting for use enjoys/forms:5.x

--- src/Crud/Setting.php
+++ src/Crud/Setting.php
@@ -8,8 +8,9 @@ use App\Module\Admin\Core\ModelInterface;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ObjectRepository;
+use Enjoys\Forms\AttributeFactory;
 use Enjoys\Forms\Form;
-use Enjoys\Forms\Renderer\RendererInterface;
+use Enjoys\Forms\Interfaces\RendererInterface;
 use Enjoys\ServerRequestWrapper;
 use EnjoysCMS\Core\Components\Helpers\Redirect;
 use EnjoysCMS\Module\Catalog\Entities\OptionKey;
@@ -33,17 +34,16 @@ final class Setting implements ModelInterface
         if ($form->isSubmitted()) {
             $this->doAction();
         }
+
+        $this->renderer->setForm($form);
         return [
-            'form' => $form->render($this->renderer)
+            'form' => $this->renderer->output()
         ];
     }
 
     private function getForm(): Form
     {
-        $form = new Form(['method' => 'post']);
-
-
-
+        $form = new Form();
 
         $form->setDefaults(function (){
             $setting = $this->setting->findAll();
@@ -62,7 +62,7 @@ final class Setting implements ModelInterface
             'searchOptionField',
             "Опции по которым также будет идти поиск"
         )
-            ->setAttribute('id', 'searchOptionField')
+            ->setAttr(AttributeFactory::create('id', 'searchOptionField'))
             ->setDescription(
                 'Опции по которым также будет идти поиск, наряду с названием, описанием и названием категории.
                 Берутся из параметров товара (опций)'
