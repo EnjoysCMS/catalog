4281e295ff808a09d8995dfda29aa99938803aa4
enjoys <deadly.info@gmail.com>
2022-07-17 23:46:44 +0300

    manage files with flysystem

--- src/Crud/Product/Files/Upload.php
+++ src/Crud/Product/Files/Upload.php
@@ -113,6 +113,7 @@ final class Upload implements ModelInterface
             $productFile->setOriginalFilename($file->getOriginalFilename());
             $productFile->setDescription($this->requestWrapper->getPostData('description'));
             $productFile->setTitle($this->requestWrapper->getPostData('title'));
+            $productFile->setStorage($this->config->get('productFileStorage'));
 
             $this->em->persist($productFile);
             $this->em->flush();
