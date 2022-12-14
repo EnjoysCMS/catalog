19f8cd73e7cd4b2ba18cdf66f919e4078b685d05
Enjoys <deadly.info@gmail.com>
2022-12-14 14:54:12 +0300

    Added  twig function callstatic

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -13,6 +13,7 @@ use EnjoysCMS\Module\Catalog\Config;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Twig\Environment;
+use Twig\TwigFunction;
 
 abstract class PublicController extends BaseController
 {
@@ -34,6 +35,18 @@ abstract class PublicController extends BaseController
 
         $this->twig->addGlobal('module', $this->module);
         $this->twig->addGlobal('config', $this->config);
+
+        $this->twig->addFunction(
+            new TwigFunction('callstatic', function ($class, $method, ...$args) {
+                if (!class_exists($class)) {
+                    throw new \Exception("Cannot call static method $method on Class $class: Invalid Class");
+                }
+                if (!method_exists($class, $method)) {
+                    throw new \Exception("Cannot call static method $method on Class $class: Invalid method");
+                }
+                return forward_static_call_array([$class, $method], $args);
+            })
+        );
     }
 
 }
