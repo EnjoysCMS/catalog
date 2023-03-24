71635ede1e0836a746e626624f706d47625ff9cc
enjoys <deadly.info@gmail.com>
2023-03-24 18:32:26 +0300

    refactor: use api for `/admin/catalog/products`

--- src/Controller/Admin/Api/ProductController.php
+++ src/Controller/Admin/Api/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace EnjoysCMS\Module\Catalog\Controller\Admin\Api;
 
-use EnjoysCMS\Module\Catalog\Crud\Product\Index;
+use EnjoysCMS\Core\Exception\NotFoundException;
 use EnjoysCMS\Module\Catalog\Entities\Category;
 use EnjoysCMS\Module\Catalog\Entities\Product;
 use EnjoysCMS\Module\Catalog\Service\ProductService;
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\EncoderInterface;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -38,6 +39,10 @@ class ProductController
         $this->normalizers = [new ObjectNormalizer()];
     }
 
+    /**
+     * @throws ExceptionInterface
+     * @throws NotFoundException
+     */
     #[Route(
         path: 'admin/catalog/api/get-products',
         name: 'catalog/admin/api/get-products',
@@ -51,41 +56,46 @@ class ProductController
     public function getProducts(ProductService $productsService): ResponseInterface
     {
         $serializer = new Serializer($this->normalizers, $this->encoders);
-        $page = $this->request->getQueryParams()['page'] ?? 1;
-        $limit = $this->request->getQueryParams()['limit'] ?? 10;
+        $limit = (int)($this->request->getQueryParams()['length'] ?? 10);
+        $page = ((int)($this->request->getQueryParams()['start'] ?? 0) / $limit) + 1;
         $products = $productsService->getProducts($page, $limit);
 
         $this->response->getBody()->write(
-            $serializer->serialize($products, JsonEncoder::FORMAT, [
-                AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
-                    return match ($object::class){
-                        Category::class => $object->getTitle(),
-                        Product::class => $object->getName(),
-                    };
-                },
-                AbstractNormalizer::CIRCULAR_REFERENCE_LIMIT => 1,
-                AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
-                AbstractNormalizer::ATTRIBUTES => [
-                    'id',
-                    'name',
-                    'productCode',
-                    'hide',
-                    'active',
-                    'category' => [
-                        'title',
+            json_encode([
+                'draw' => $this->request->getQueryParams()['draw'] ?? null,
+                'recordsTotal' => $products['pagination']->getTotalItems(),
+                'recordsFiltered' => $products['pagination']->getTotalItems(),
+                'data' => $serializer->normalize($products['products'], JsonEncoder::FORMAT, [
+                    AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
+                        return match ($object::class) {
+                            Category::class => $object->getTitle(),
+                            Product::class => $object->getName(),
+                        };
+                    },
+                    AbstractNormalizer::CIRCULAR_REFERENCE_LIMIT => 1,
+                    AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
+                    AbstractNormalizer::ATTRIBUTES => [
+                        'id',
+                        'name',
+                        'productCode',
+                        'hide',
+                        'active',
+                        'category' => [
+                            'title',
+                            'slug',
+                            'breadcrumbs'
+                        ],
                         'slug',
-                        'breadcrumbs'
-                    ],
-                    'slug',
-                    'defaultImage' => [
-                        'filename',
-                        'extension',
-                        'storage'
-                    ],
-
-                ]
+                        'defaultImage' => [
+                            'filename',
+                            'extension',
+                            'storage'
+                        ],
+                    ]
+                ])
             ])
         );
+
         return $this->response;
     }
 }
