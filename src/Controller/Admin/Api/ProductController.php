ac3297af554a37f1744a61150f274d1b7a724266
enjoys <deadly.info@gmail.com>
2023-03-24 09:38:20 +0300

    Added `api/get-products`

---
+++ src/Controller/Admin/Api/ProductController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace EnjoysCMS\Module\Catalog\Controller\Admin\Api;
+
+use EnjoysCMS\Module\Catalog\Crud\Product\Index;
+use EnjoysCMS\Module\Catalog\Entities\Category;
+use EnjoysCMS\Module\Catalog\Entities\Product;
+use EnjoysCMS\Module\Catalog\Service\ProductService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\EncoderInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class ProductController
+{
+
+    /**
+     * @var EncoderInterface[]
+     */
+    private array $encoders;
+    /**
+     * @var AbstractNormalizer[]
+     */
+    private array $normalizers;
+
+    public function __construct(
+        private ServerRequestInterface $request,
+        private ResponseInterface $response
+    ) {
+        $this->response = $this->response->withHeader('content-type', 'application/json');
+        $this->encoders = [new XmlEncoder(), new JsonEncoder()];
+        $this->normalizers = [new ObjectNormalizer()];
+    }
+
+    #[Route(
+        path: 'admin/catalog/api/get-products',
+        name: 'catalog/admin/api/get-products',
+        options: [
+            'comment' => 'API: получение списка продуктов'
+        ],
+        methods: [
+            'GET'
+        ]
+    )]
+    public function getProducts(ProductService $productsService): ResponseInterface
+    {
+        $serializer = new Serializer($this->normalizers, $this->encoders);
+        $page = $this->request->getQueryParams()['page'] ?? 1;
+        $limit = $this->request->getQueryParams()['limit'] ?? 10;
+        $products = $productsService->getProducts($page, $limit);
+
+        $this->response->getBody()->write(
+            $serializer->serialize($products, JsonEncoder::FORMAT, [
+                AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
+                    return match ($object::class){
+                        Category::class => $object->getTitle(),
+                        Product::class => $object->getName(),
+                    };
+                },
+                AbstractNormalizer::CIRCULAR_REFERENCE_LIMIT => 1,
+                AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
+                AbstractNormalizer::ATTRIBUTES => [
+                    'id',
+                    'name',
+                    'productCode',
+                    'hide',
+                    'active',
+                    'category' => [
+                        'title',
+                        'slug',
+                        'breadcrumbs'
+                    ],
+                    'slug',
+                    'defaultImage' => [
+                        'filename',
+                        'extension',
+                        'storage'
+                    ],
+
+                ]
+            ])
+        );
+        return $this->response;
+    }
+}
---
+++ src/Service/ProductService.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EnjoysCMS\Module\Catalog\Service;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use EnjoysCMS\Core\Components\Pagination\Pagination;
+use EnjoysCMS\Module\Catalog\Entities\Product;
+use EnjoysCMS\Module\Catalog\Repositories;
+
+final class ProductService
+{
+
+    private Repositories\Product|EntityRepository $productRepository;
+
+    public function __construct(
+        private EntityManager $em
+    ) {
+        $this->productRepository = $this->em->getRepository(Product::class);
+    }
+
+
+    public function getProducts(int $page = 1, int $limit = 10): array
+    {
+        $pagination = new Pagination($page, $limit);
+        $qb = $this->productRepository->getFindAllBuilder();
+        $query = $qb->orderBy('p.id', 'desc')
+            ->getQuery()
+            ->setFirstResult($pagination->getOffset())
+            ->setMaxResults($pagination->getLimitItems());
+
+        $result = new Paginator($query);
+        $pagination->setTotalItems($result->count());
+        return [
+            'products' => $result,
+            'pagination' => $pagination,
+        ];
+    }
+}
