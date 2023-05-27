94f791369c5021b20071784a8a8c57c1a98d4e89
enjoys <deadly.info@gmail.com>
2023-05-28 00:11:19 +0300

    filters form

--- src/Blocks/Filter.php
+++ src/Blocks/Filter.php
@@ -7,15 +7,14 @@ use DI\DependencyException;
 use DI\NotFoundException;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\Expr;
+use Enjoys\Forms\Form;
+use Enjoys\Forms\Interfaces\RendererInterface;
 use EnjoysCMS\Core\Components\Blocks\AbstractBlock;
 use EnjoysCMS\Core\Entities\Block as Entity;
 use EnjoysCMS\Module\Catalog\Entities\Category;
-use EnjoysCMS\Module\Catalog\Entities\OptionKey;
 use EnjoysCMS\Module\Catalog\Entities\OptionValue;
 use EnjoysCMS\Module\Catalog\Entities\Product;
 use EnjoysCMS\Module\Catalog\Repositories\FilterRepository;
-use EnjoysCMS\Module\Catalog\Repositories\OptionValueRepository;
 use Psr\Http\Message\ServerRequestInterface;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -59,41 +58,55 @@ class Filter extends AbstractBlock
         $request = $this->container->get(ServerRequestInterface::class);
         $slug = $request->getAttribute('slug', '');
         $category = $categoryRepository->findByPath($slug);
-//        $filters = $filterRepository->findBy(['category' => $category]);
-//
-//        /** @var int[] $pids id всех продуктов в этой категории */
-//        $pids = array_column(
-//            $productRepository->getFindAllBuilder()
-//                ->select('p.id')
-//                ->where('c.id = :category')
-//                ->setParameter('category', $category)
-//                ->distinct()
-//                ->getQuery()->getArrayResult(),
-//            'id'
-//        );
-//
-//
-//        $values = $em->getRepository(OptionValue::class)->createQueryBuilder('v')
-//            ->leftJoin(OptionKey::class, 'k', Expr\Join::WITH, 'k.id = v.optionKey')
-//            ->leftJoin(\EnjoysCMS\Module\Catalog\Entities\Filter::class, 'f', Expr\Join::WITH, 'f.optionKey = k.id')
-//            ->where('f.category = :category')
-//            ->andWhere(':pids MEMBER OF v.products')
-//            ->setParameters([
-//                'pids' => $pids,
-//                'category' => $category
-//            ])
-//            ->getQuery()
-//            ->getResult();
+        /** @var \EnjoysCMS\Module\Catalog\Entities\Filter[] $allowedFilters */
+        $allowedFilters = $filterRepository->findBy(['category' => $category]);
 
-        $filters = $filterRepository->getCategoryFilters($category);
-        dump($filters);
+        /** @var int[] $pids id всех продуктов в этой категории */
+        $pids = array_column(
+            $em->createQueryBuilder()
+                ->select('p.id')
+                ->from(Product::class, 'p')
+                ->leftJoin('p.category', 'c')
+                ->where('p.category IN (:category)')
+                ->distinct()
+                ->setParameter('category', $categoryRepository->getAllIds($category))
+                ->getQuery()
+                ->getArrayResult(),
+            'id'
+        );
+
+//        $filters = new \SplObjectStorage();
+//        foreach ($allowedFilters as $filter) {
+//            $filters[$filter] = $filterRepository->getValues($filter->getOptionKey(), $pids);
+//        }
+
+        $form = new Form('get');
+        foreach ($allowedFilters as $filter) {
+            $form->checkbox('filter[]', $filter->getOptionKey()->__toString())
+                ->fill(function () use ($filter, $filterRepository, $pids) {
+                    $result = [];
+                    /** @var OptionValue $value */
+                    foreach ($filterRepository->getValues($filter->getOptionKey(), $pids) as $value) {
+                        $result[$value->getId()] = $value->getValue();
+                    }
+                    return $result;
+                });
+        }
+
+
+
+        $form->submit('submit1', 'Показать')->removeAttribute('name');
+
+        $renderForm = $this->container->get(RendererInterface::class);
+        $renderForm->setForm($form);
 
         return $this->twig->render(
             empty($this->getOption('template')) ? '../modules/catalog/template/blocks/filter.twig' : $this->getOption(
                 'template'
             ),
             [
-                'filters' => $filters,
+//                'filters' => $filters,
+                'form' => $renderForm->output(),
                 'blockOptions' => $this->getOptions()
             ]
         );
--- src/Repositories/FilterRepository.php
+++ src/Repositories/FilterRepository.php
@@ -2,56 +2,26 @@
 
 namespace EnjoysCMS\Module\Catalog\Repositories;
 
-use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\Expr;
-use EnjoysCMS\Module\Catalog\Entities\Category;
-use EnjoysCMS\Module\Catalog\Entities\Filter;
 use EnjoysCMS\Module\Catalog\Entities\OptionKey;
 use EnjoysCMS\Module\Catalog\Entities\OptionValue;
-use EnjoysCMS\Module\Catalog\Entities\Product;
-use EnjoysCMS\Module\Catalog\ORM\Doctrine\FilterHydrate;
 
 class FilterRepository extends EntityRepository
 {
 
-    public function getCategoryFilters(Category $category)
-    {
-        $this->getEntityManager()->getConfiguration()->addCustomHydrationMode(
-            FilterHydrate::NAME,
-            FilterHydrate::class
-        );
 
-        $qb = $this->createQueryBuilder('f')
-            ->select('f, v, k')
-            ->join(OptionKey::class, 'k', Expr\Join::WITH, 'k.id = f.optionKey')
-            ->join(OptionValue::class, 'v', Expr\Join::WITH, 'k.id = v.optionKey')
-            ->where('f.category = :category')
+    public function getValues(OptionKey $optionKey, array $pids)
+    {
+        return $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('v')
+            ->from(OptionValue::class, 'v')
+            ->where('v.optionKey = :optionKey')
             ->andWhere(':pids MEMBER OF v.products')
-//            ->groupBy('k.id')
             ->setParameters([
-                'pids' => $this->getProductIdsFromCategory($category),
-                'category' => $category
-            ]);
-
-        return $qb->getQuery()->getResult(FilterHydrate::NAME);
-    }
-
-    private function getProductIdsFromCategory(Category $category): array
-    {
-        return array_column(
-            $this
-                ->getEntityManager()
-                ->createQueryBuilder()->select('p.id')
-                ->from(Product::class, 'p')
-                ->leftJoin('p.category', 'c')
-                ->where('c.id = :category')
-                ->distinct()
-                ->setParameter('category', $category)
-                ->getQuery()
-                ->getArrayResult(),
-            'id'
-        );
+                'pids' => $pids,
+                'optionKey' => $optionKey
+            ])->getQuery()->getResult();
     }
 
 }
