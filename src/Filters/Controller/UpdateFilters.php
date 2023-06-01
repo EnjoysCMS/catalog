a94f3d719d310376a65104f34255fc2dd89b5b16
enjoys <deadly.info@gmail.com>
2023-06-01 10:09:05 +0300

    crud: update filters (order)

--- src/Filters/Controller/UpdateFilters.php
+++ src/Filters/Controller/UpdateFilters.php
@@ -2,7 +2,52 @@
 
 namespace EnjoysCMS\Module\Catalog\Filters\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\NotSupported;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use EnjoysCMS\Module\Catalog\Entities\Category;
+use EnjoysCMS\Module\Catalog\Entities\Filter;
+use EnjoysCMS\Module\Catalog\Filters\Entity\FilterEntity;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(
+    path: 'admin/catalog/filter',
+    name: 'catalog/filter/update',
+    methods: [
+        'PATCH'
+    ]
+)]
 class UpdateFilters
 {
+    private \stdClass $input;
 
+    public function __construct(
+        private ServerRequestInterface $request,
+        private ResponseInterface $response,
+        private EntityManager $em,
+    ) {
+        $this->input = json_decode($this->request->getBody()->getContents());
+        $this->response = $this->response->withHeader('content-type', 'application/json');
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws NotSupported
+     * @throws ORMException
+     */
+    public function __invoke(): ResponseInterface
+    {
+        /** @var Category $category */
+        $filter = $this->em->getRepository(FilterEntity::class)->find(
+            $this->input->filterId ?? throw new \InvalidArgumentException('Filter id not found')
+        ) ?? throw new \RuntimeException('Filter not found');
+
+        $filter->setOrder((int)($this->input->order ?? 0));
+
+        $this->em->flush();
+        return $this->response;
+    }
 }
