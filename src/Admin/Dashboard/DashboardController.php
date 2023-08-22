f6ac5ca4414ed843837177a0ae2d5e66b46eb7dc
enjoys <deadly.info@gmail.com>
2023-08-22 10:11:46 +0300

    move dashboard controller

--- src/Admin/Dashboard/DashboardController.php
+++ src/Admin/Dashboard/DashboardController.php
@@ -2,24 +2,23 @@
 
 declare(strict_types=1);
 
-namespace EnjoysCMS\Module\Catalog\Controller\Admin;
+namespace EnjoysCMS\Module\Catalog\Admin\Dashboard;
 
 use EnjoysCMS\Core\Routing\Annotation\Route;
 use EnjoysCMS\Module\Catalog\Admin\AdminController;
 use Psr\Http\Message\ResponseInterface;
 
 #[Route(
-    path: "admin/catalog",
+    path: "/admin/catalog",
     name: '@catalog_admin'
 )]
-final class Dashboard extends AdminController
+final class DashboardController extends AdminController
 {
     public function __invoke(): ResponseInterface
     {
         return $this->response(
             $this->twig->render(
-                $this->templatePath . '/dashboard.twig',
-                []
+                $this->templatePath . '/dashboard.twig'
             )
         );
     }
