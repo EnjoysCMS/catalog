00956accca370592877b3f0897d96de2c6d75913
enjoys <deadly.info@gmail.com>
2023-08-31 18:37:55 +0300

    Revert "post-package-uninstall"
    This reverts commit e33d334119dff95ee722eb3b664ffb1d1a7fd173.

--- install/Composer.php
+++ install/Composer.php
@@ -3,9 +3,9 @@
 namespace EnjoysCMS\Module\Catalog\Install;
 
 use App\Install\Functions\CommandsManage;
-use Composer\EventDispatcher\Event as BaseEvent;
 use Composer\Installer\PackageEvent;
 use Composer\Script\Event;
+use Composer\EventDispatcher\Event as BaseEvent;
 use EnjoysCMS\Module\Catalog\Command\CurrencyRate;
 use Exception;
 
@@ -15,6 +15,11 @@ class Composer
     private static array $commands = [
         CurrencyRate::class => []
     ];
+
+    private static function getRootPath(BaseEvent $event): string
+    {
+        return dirname($event->getComposer()->getConfig()->getConfigSource()->getName());
+    }
 
     public static function assetsInstall(): void
     {
@@ -26,9 +31,23 @@ class Composer
      */
     public static function registerCommands(Event $event): void
     {
-        $manage = new CommandsManage(dirname($event->getComposer()->getConfig()->getConfigSource()->getName()) . '/console.yml');
+        $manage = new CommandsManage(self::getRootPath($event) . '/console.yml');
         $manage->registerCommands(self::$commands);
         $manage->save();
     }
 
+    /**
+     * @throws Exception
+     */
+    public static function unregisterCommands(PackageEvent $event): void
+    {
+        $event->stopPropagation();
+        if ($event->getOperation()->getPackage()->getName() !== "enjoyscms/catalog"){
+            return;
+        }
+        $manage = new CommandsManage(self::getRootPath($event) . '/console.yml');
+        $manage->unregisterCommands(array_keys(self::$commands));
+        $manage->save();
+
+    }
 }
