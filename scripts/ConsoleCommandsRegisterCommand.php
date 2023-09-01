3b2e734c77bb30e21190612e426a679a97bfbb7f
enjoys <deadly.info@gmail.com>
2023-09-01 16:12:10 +0300

    refactor commands

--- scripts/ConsoleCommandsRegisterCommand.php
+++ scripts/ConsoleCommandsRegisterCommand.php
@@ -2,41 +2,15 @@
 
 namespace EnjoysCMS\Module\Catalog\Composer\Scripts;
 
-use Enjoys\Config\Config;
-use EnjoysCMS\Core\Console\Utils\CommandsManage;
+use EnjoysCMS\Core\Composer\Scripts\RegisterCommandsToConsoleCommand;
 use EnjoysCMS\Module\Catalog\Command\CurrencyRate;
-use Exception;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
-class ConsoleCommandsRegisterCommand extends Command
+class ConsoleCommandsRegisterCommand extends RegisterCommandsToConsoleCommand
 {
+    protected string $moduleName = 'Catalog';
 
-    private array $commands = [
+    protected array $commands = [
         CurrencyRate::class => []
     ];
 
-    /**
-     * @throws Exception
-     */
-    public function execute(InputInterface $input, OutputInterface $output): int
-    {
-        if ($this->commands === []){
-            return Command::SUCCESS;
-        }
-        $container = include __DIR__ . '/../../../bootstrap.php';
-        $commandManage = new CommandsManage(config: $container->get(Config::class));
-        $registeredCommands = $commandManage->registerCommands($this->commands);
-        $changed = false;
-        $output->writeln('<info>Register console commands [Catalog]</info>');
-        foreach ($registeredCommands as $command) {
-            $output->writeln(sprintf(' <options=bold>- %s</></info>', $command));
-            $changed = true;
-        }
-        if ($changed) {
-            $commandManage->save();
-        }
-        return Command::SUCCESS;
-    }
 }
