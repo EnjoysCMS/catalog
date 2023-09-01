4a99578d22019cef66c6276cd5c305738af6cd01
enjoys <deadly.info@gmail.com>
2023-09-01 16:26:41 +0300

    improved composer scripts

--- scripts/AssetsInstallCommand.php
+++ scripts/AssetsInstallCommand.php
@@ -2,34 +2,9 @@
 
 namespace EnjoysCMS\Module\Catalog\Composer\Scripts;
 
-use Exception;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
+use EnjoysCMS\Core\Console\Command\AbstractAssetsInstallCommand;
 
-class AssetsInstallCommand extends Command
+class AssetsInstallCommand extends AbstractAssetsInstallCommand
 {
-
-    /**
-     * @throws Exception
-     */
-    public function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $process = new Process([
-            'yarn',
-            'install',
-        ], cwd: realpath(__DIR__ . '/..'));
-
-        $output->writeln(sprintf("<info>%s</info>", $process->getWorkingDirectory()));
-
-        $process->setTimeout(60);
-
-        $process->run(
-            function ($type, $buffer) use ($output) {
-                $output->write((Process::ERR === $type) ? 'ERR:' . $buffer : $buffer);
-            }
-        );
-        return Command::SUCCESS;
-    }
+    protected string $cwd = __DIR__ . '/..';
 }
--- scripts/ConsoleCommandsRegisterCommand.php
+++ scripts/ConsoleCommandsRegisterCommand.php
@@ -2,10 +2,10 @@
 
 namespace EnjoysCMS\Module\Catalog\Composer\Scripts;
 
-use EnjoysCMS\Core\Composer\Scripts\RegisterCommandsToConsoleCommand;
+use EnjoysCMS\Core\Console\Command\AbstractCommandsRegister;
 use EnjoysCMS\Module\Catalog\Command\CurrencyRate;
 
-class ConsoleCommandsRegisterCommand extends RegisterCommandsToConsoleCommand
+class ConsoleCommandsRegisterCommand extends AbstractCommandsRegister
 {
     protected string $moduleName = 'Catalog';
 
