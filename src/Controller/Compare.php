ad56be4017aabbe3f29ac0e1df2790a522172d8e
enjoys <deadly.info@gmail.com>
2023-12-28 13:39:05 +0300

    auto clear old compare lists

--- src/Controller/Compare.php
+++ src/Controller/Compare.php
@@ -6,13 +6,17 @@ namespace EnjoysCMS\Module\Catalog\Controller;
 
 use DI\DependencyException;
 use DI\NotFoundException;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\NotSupported;
 use Enjoys\Cookie\Cookie;
 use Enjoys\Cookie\Exception;
 use EnjoysCMS\Core\AbstractController;
+use EnjoysCMS\Core\Auth\Identity;
 use EnjoysCMS\Core\Routing\Annotation\Route;
+use EnjoysCMS\Core\Users\Entity\User;
 use EnjoysCMS\Module\Catalog\Config;
 use EnjoysCMS\Module\Catalog\Entity\CompareList;
 use EnjoysCMS\Module\Catalog\Service\Compare\GoodsComparator;
@@ -28,10 +32,21 @@ final class Compare extends AbstractController
 {
 
     /**
+     * @throws \Exception
+     */
+    #[Route('catalog/compare/count', 'catalog_compare_count', priority: 3)]
+    public function getCountGoodsInCompareList(Identity $identity): ResponseInterface
+    {
+        $compareList = $this->getCompareList($identity->getUser());
+        return $this->json(\count($compareList?->getGoodsIds() ?? []));
+    }
+
+    /**
      * @throws Exception
+     * @throws \Exception
      */
     #[Route('catalog/compare/add', 'catalog_compare_add', priority: 3)]
-    public function add(Cookie $cookie, EntityManagerInterface $em): ResponseInterface
+    public function add(Cookie $cookie, EntityManagerInterface $em, Identity $identity): ResponseInterface
     {
         $parsedBody = json_decode($this->request->getBody()->getContents());
 
@@ -41,11 +56,17 @@ final class Compare extends AbstractController
             return $this->json(['error' => true, 'message' => 'Invalid product id'], 400);
         }
 
-        $compareList = $this->getCompareList();
+        $user = $identity->getUser();
+
+        $compareList = $this->getCompareList($user);
 
         if ($compareList === null) {
             $compareList = new CompareList();
             $compareList->setId(Uuid::uuid4()->toString());
+            if (!$user->isGuest()) {
+                $compareList->setUser($user);
+            }
+
             $em->persist($compareList);
         }
 
@@ -71,10 +92,14 @@ final class Compare extends AbstractController
      * @throws DependencyException
      * @throws NotFoundException
      * @throws NotSupported
+     * @throws \Exception
      */
     #[Route('catalog/compare', 'catalog_compare', priority: 3)]
-    public function compare(GoodsComparator $goodsComparator, Config $config): ResponseInterface
+    public function compare(GoodsComparator $goodsComparator, Identity $identity, Config $config): ResponseInterface
     {
+        // 5%
+        $this->gc(50000);
+
         $this->breadcrumbs->add('catalog/index', 'Каталог')->add(title: 'Сравнение товаров');
 
         /** @var \EnjoysCMS\Module\Catalog\Repository\Product $repo */
@@ -83,7 +108,7 @@ final class Compare extends AbstractController
         );
 
 
-        $compareList = $this->getCompareList();
+        $compareList = $this->getCompareList($identity->getUser());
 
         /** @var \EnjoysCMS\Module\Catalog\Entity\Product[] $products */
         $products = $repo->findByIds($compareList?->getGoodsIds() ?? []);
@@ -102,14 +127,29 @@ final class Compare extends AbstractController
     }
 
 
-    private function getCompareList(): ?CompareList
+    private function getCompareList(User $user): ?CompareList
     {
-        $compareListId = $this->request->getCookieParams()['compare-list-id'] ?? null;
-        $em = $this->container->get(EntityManagerInterface::class);
-        $repository = $em->getRepository(CompareList::class);
-        return $repository->findOneBy([
-            'id' => $compareListId
-        ]);
+        try {
+            $compareListId = $this->request->getCookieParams()['compare-list-id'] ?? '';
+            $em = $this->container->get(EntityManagerInterface::class);
+            $repository = $em->getRepository(CompareList::class);
+            if ($user->isGuest()) {
+                /** @var null|CompareList $compareList */
+                $compareList = $repository->find($compareListId);
+                if ($compareList === null) {
+                    return null;
+                }
+                if (new \DateTimeImmutable('-7 days') > $compareList->getCreatedAt()) {
+                    $em->remove($compareList);
+                    $em->flush();
+                    return null;
+                }
+                return $compareList;
+            }
+            return $repository->findOneBy(['user' => $user]);
+        } catch (ConversionException) {
+            return null;
+        }
     }
 
     private function getProduct(EntityManagerInterface $em, ?string $id): ?\EnjoysCMS\Module\Catalog\Entity\Product
@@ -119,5 +159,39 @@ final class Compare extends AbstractController
         }
         return $em->getRepository(\EnjoysCMS\Module\Catalog\Entity\Product::class)->find($id);
     }
+
+    /**
+     * Garbage Collector
+     *
+     * @param int $gcProbability gcProbability the probability (parts per million) that garbage
+     * collection (GC) should be performed when storing a piece of data in the cache.
+     * Defaults to 10, meaning 0.001% chance. This number should be between 0 and 1000000.
+     * A value 0 means no GC will be performed at all.
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    private function gc(int $gcProbability = 10): void
+    {
+        if (\random_int(0, 1000000) < $gcProbability) {
+            $em = $this->container->get(EntityManagerInterface::class);
+            $repository = $em->getRepository(CompareList::class);
+
+            $criteria = new Criteria();
+            $criteria
+                ->where(Criteria::expr()->eq('user', null))
+                ->andWhere(
+                    Criteria::expr()->lt('createdAt', new \DateTimeImmutable('-7 days'))
+                );
+
+            $result = $repository->matching($criteria);
+
+            foreach ($result as $object) {
+                $em->remove($object);
+            }
+
+            $em->flush();
+        }
+    }
+
 
 }
